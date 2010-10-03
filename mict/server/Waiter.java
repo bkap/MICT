1ab8721e57bcf1c9b5950fc685085f80dfc448b1
Rob Wiesler <halcyone1024@gmail.com>
2010-10-03 14:29:41 -0400

    Minor changes. Also leveled .gitignore for LaTeX crap.

--- mict/server/Waiter.java
+++ mict/server/Waiter.java
@@ -4,16 +4,16 @@ import javax.net.ssl.*;
 import java.io.*;
 
 public class Waiter extends Thread {
-	public Waiter(SSLSocket patron, Server server) throws IOException {
+	public Waiter(SSLSocket patron, Server parent) throws IOException {
 		this.patron = patron;
-		this.server = server;
+		this.parent = parent;
 		out = new PrintWriter(new OutputStreamWriter(patron.getOutputStream()));
 		in = new BufferedReader(new InputStreamReader(patron.getInputStream()));
 		setDaemon(true);
 	}
 
 	private SSLSocket patron;
-	private Server server;
+	private Server parent;
 	private PrintWriter out;
 	private BufferedReader in;
 
@@ -21,7 +21,8 @@ public class Waiter extends Thread {
 		// DO WORK, SON
 	}
 
-	public void close() {
+	protected void close() {
+		parent.clients.remove(this);
 		try {
 			patron.close();
 			in.close();
