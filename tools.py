579537a24782e59c91f9361890b3abbc1621a656
Donald Huckle <drh32@case.edu>
2010-10-20 15:45:28 -0400

    Added the drawRectangle and filled Oval tools.  Mark you may now do the immages.

--- tools.py
+++ tools.py
@@ -122,6 +122,92 @@ class RectangleTool(Tool) :
 		self.image = BufferedImage(32,32,BufferedImage.TYPE_INT_ARGB)
 		g = self.image.getGraphics()
 		g.setColor(Color(0,0,0))
+		g.drawRect(0,0,32,32)
+
+	def mousePressed(self, locationOnScreen, g) :
+		self.start_point = locationOnScreen
+		return ""
+
+	def mouseHovered(self, locationOnScreen, g) :
+		return ""
+
+	def mouseDragged(self, locationOnScreen, g) :
+		x1 = min(self.start_point.x, locationOnScreen.x)
+		y1 = min(self.start_point.y, locationOnScreen.y)
+		x2 = max(self.start_point.x, locationOnScreen.x)
+		y2 = max(self.start_point.y, locationOnScreen.y)
+		g.setColor(self.client_state.selectedColor)
+		g.drawRect(x1, y1, x2 - x1, y2 - y1)
+		return ""
+
+	def mouseReleased(self, locationOnScreen, g) :
+		x1 = min(self.start_point.x, locationOnScreen.x)
+		y1 = min(self.start_point.y, locationOnScreen.y)
+		x2 = max(self.start_point.x, locationOnScreen.x)
+		y2 = max(self.start_point.y, locationOnScreen.y)
+		return self._getmetadata() + "|" + "(%d,%d);(%d,%d) " % (x1, y1, x2 - x1, y2 - y1)
+	def _getmetadata(self) :
+		return "%d" % self.client_state.selectedColor.getRGB()
+	
+	def draw(self, s, g) :
+		if s == "" :
+			return
+		metadata, points = s.split('|')
+		points = points.split(';')
+		g.setColor(Color(int(metadata)))
+		match = point_re.match(points[0])
+		if match is None:
+			return
+		x1, y1 = match.groups()
+		x1, y1 = int(x1), int(y1)
+		match = point_re.match(points[1])
+		if match is None:
+			return
+		x2, y2 = match.groups()
+		x2, y2 = int(x2), int(y2)
+		g.drawRect(x1, y1, x2, y2)
+
+	def getAffectedArea(self, phrase) :
+		points = phrase.split('|')[1].split(';')
+		match = point_re.match(points[0])
+		if match is None:
+			return
+		x1, y1 = match.groups()
+		x1, y1 = int(x1), int(y1)
+		match = point_re.match(points[1])
+		if match is None:
+			return
+		x2, y2 = match.groups()
+		x2, y2 = int(x2), int(y2)
+		ax1 = min(x1, x2)
+		ay1 = min(y1, y2)
+		ax2 = max(x1, x2)
+		ay2 = max(y1, y2)
+		return [ax1, ay1, ax2 - ax1, ay2 - ay1]
+
+	def getIcon(self) :
+		return self.image
+
+	def getToolName(self) :
+		return "Rectangle"
+
+	def getTooltip(self) :
+		return "draw a rectangle with one corner\nwhere you click and the opposite\ncorner where you release the mouse"
+
+	def getToolID(self) :
+		return 'rect'   
+
+
+class FilledRectangleTool(Tool) :
+	def __init__(self, clientState = None) :
+		self.client_state = clientState
+		self.start_point = None
+		self.makeImage()
+
+	def makeImage(self) :
+		self.image = BufferedImage(32,32,BufferedImage.TYPE_INT_ARGB)
+		g = self.image.getGraphics()
+		g.setColor(Color(0,0,0))
 		g.fillRect(0,0,32,32)
 
 	def mousePressed(self, locationOnScreen, g) :
@@ -189,13 +275,13 @@ class RectangleTool(Tool) :
 		return self.image
 
 	def getToolName(self) :
-		return "Rectangle"
+		return "Filled Rectangle"
 
 	def getTooltip(self) :
-		return "draw a rectangle with one corner\nwhere you click and the opposite\ncorner where you release the mouse"
+		return "draw a filled rectangle with one corner\nwhere you click and the opposite\ncorner where you release the mouse"
 
 	def getToolID(self) :
-		return 'rect'   
+		return 'fillRect'   
 
 class LineTool(Tool) :
 	def __init__(self, clientState = None) :
@@ -286,6 +372,91 @@ class LineTool(Tool) :
 
 	def getToolID(self) :
 		return 'line'	
+class FilledOvalTool(Tool) :
+	def __init__(self, clientState = None) :
+		self.client_state = clientState
+		self.start_point = None
+		self.makeImage()
+
+	def makeImage(self) :
+		self.image = BufferedImage(32,32,BufferedImage.TYPE_INT_ARGB)
+		g = self.image.getGraphics()
+		g.setColor(Color(0,0,0))
+		g.fillOval(0,0,30,30)
+
+	def mousePressed(self, locationOnScreen, g) :
+		self.start_point = locationOnScreen
+		return ""
+
+	def mouseHovered(self, locationOnScreen, g) :
+		return ""
+
+	def mouseDragged(self, locationOnScreen, g) :
+		x1 = min(self.start_point.x, locationOnScreen.x)
+		y1 = min(self.start_point.y, locationOnScreen.y)
+		x2 = max(self.start_point.x, locationOnScreen.x)
+		y2 = max(self.start_point.y, locationOnScreen.y)
+		g.setColor(self.client_state.selectedColor)		
+		g.fillOval(x1, y1, x2 - x1, y2 - y1)
+		return ""
+
+	def mouseReleased(self, locationOnScreen, g) :
+		x1 = min(self.start_point.x, locationOnScreen.x)
+		y1 = min(self.start_point.y, locationOnScreen.y)
+		x2 = max(self.start_point.x, locationOnScreen.x)
+		y2 = max(self.start_point.y, locationOnScreen.y)
+		return self._getmetadata() + "|" + "(%d,%d);(%d,%d) " % (x1, y1, x2 - x1, y2 - y1)		
+	
+	def _getmetadata(self) :
+		return "%d" % self.client_state.selectedColor.getRGB()
+		
+	def draw(self, s, g) :
+		if s == "" :
+			return
+		metadata, points = s.split('|')
+		points = points.split(';')
+		g.setColor(Color(int(metadata)))
+		match = point_re.match(points[0])
+		if match is None:
+			return
+		x1, y1 = match.groups()
+		x1, y1 = int(x1), int(y1)
+		match = point_re.match(points[1])
+		if match is None:
+			return
+		x2, y2 = match.groups()
+		x2, y2 = int(x2), int(y2)
+		g.fillOval(x1, y1, x2, y2)
+
+	def getAffectedArea(self, phrase) :
+		points = phrase.split('|')[1].split(';')
+		match = point_re.match(points[0])
+		if match is None:
+			return
+		x1, y1 = match.groups()
+		x1, y1 = int(x1), int(y1)
+		match = point_re.match(points[1])
+		if match is None:
+			return
+		x2, y2 = match.groups()
+		x2, y2 = int(x2), int(y2)
+		ax1 = min(x1, x2)
+		ay1 = min(y1, y2)
+		ax2 = max(x1, x2)
+		ay2 = max(y1, y2)
+		return [ax1, ay1, ax2 - ax1, ay2 - ay1]
+
+	def getIcon(self) :
+		return self.image
+
+	def getToolName(self) :
+		return "FilledOval"
+
+	def getTooltip(self) :
+		return "draw a Filled Oval inscribed in the imaginary rectagle with one corner\nwhere you click and another\ncorner where you release the mouse"
+
+	def getToolID(self) :
+		return 'filledoval'	
 
 class OvalTool(Tool) :
 	def __init__(self, clientState = None) :
