b58d5f7406902513a7d0daf183dd2fec04626242
Mark Wyrzykowski <Mark@.(none)>
2010-10-20 20:41:45 -0400

    Added RoundedRectangle and FilledRoundedRectangle Tools.

--- tools.py
+++ tools.py
@@ -520,6 +520,154 @@ class FilledOvalTool(Tool) :
 	def getToolID(self) :
 		return 'filledoval'	
 
+class RoundedRectangleTool(Tool) :
+	def __init__(self, clientState = None) :
+		self.client_state = clientState
+		self.start_point = None
+		self.makeImage()
+	def makeImage(self) :
+		self.image = BufferedImage(32,32,BufferedImage.TYPE_INT_ARGB)
+		g = self.image.getGraphics()
+		g.setColor(Color(0,0,0))
+		g.drawRoundRect(0,0,31,31,10,10)
+	def mousePressed(self, locationOnScreen, g) :
+		self.start_point = locationOnScreen
+		return ""
+	def mouseHovered(self, locationOnScreen, g) :
+		return ""
+	def mouseDragged(self, locationOnScreen, g) :
+		x1 = min(self.start_point.x, locationOnScreen.x)
+		y1 = min(self.start_point.y, locationOnScreen.y)
+		x2 = max(self.start_point.x, locationOnScreen.x)
+		y2 = max(self.start_point.y, locationOnScreen.y)
+		g.setColor(self.client_state.selectedColor)
+		g.drawRoundRect(x1, y1, x2 - x1, y2 - y1, 20, 20)
+		return ""
+	def mouseReleased(self, locationOnScreen, g) :
+		x1 = min(self.start_point.x, locationOnScreen.x)
+		y1 = min(self.start_point.y, locationOnScreen.y)
+		x2 = max(self.start_point.x, locationOnScreen.x)
+		y2 = max(self.start_point.y, locationOnScreen.y)
+		return self._getmetadata() + "|" + "(%d,%d);(%d,%d) " % (x1, y1, x2 - x1, y2 - y1)
+	def _getmetadata(self) :
+		return "%d" % self.client_state.selectedColor.getRGB()
+	def draw(self, s, g) :
+		if s == "" :
+			return
+		metadata, points = s.split('|')
+		points = points.split(';')
+		g.setColor(Color(int(metadata)))
+		match = point_re.match(points[0])
+		if match is None:
+			return
+		x1, y1 = match.groups()
+		x1, y1 = int(x1), int(y1)
+		match = point_re.match(points[1])
+		if match is None:
+			return
+		x2, y2 = match.groups()
+		x2, y2 = int(x2), int(y2)
+		g.drawRoundRect(x1, y1, x2, y2, 20, 20)
+	def getAffectedArea(self, phrase) :
+		points = phrase.split('|')[1].split(';')
+		match = point_re.match(points[0])
+		if match is None:
+			return
+		x1, y1 = match.groups()
+		x1, y1 = int(x1), int(y1)
+		match = point_re.match(points[1])
+		if match is None:
+			return
+		x2, y2 = match.groups()
+		x2, y2 = int(x2), int(y2)
+		ax1 = min(x1, x2)
+		ay1 = min(y1, y2)
+		ax2 = max(x1, x2)
+		ay2 = max(y1, y2)
+		return [ax1, ay1, ax2 - ax1, ay2 - ay1]
+	def getIcon(self) :
+		return self.image
+	def getToolName(self) :
+		return "Rounded Rectangle"
+	def getTooltip(self) :
+		return "draw a rounded rectangle with one corner\nwhere you click and the opposite\ncorner where you release the mouse"
+	def getToolID(self) :
+		return 'roundrect'
+
+class FilledRoundedRectangleTool(Tool) :
+	def __init__(self, clientState = None) :
+		self.client_state = clientState
+		self.start_point = None
+		self.makeImage()
+	def makeImage(self) :
+		self.image = BufferedImage(32,32,BufferedImage.TYPE_INT_ARGB)
+		g = self.image.getGraphics()
+		g.setColor(Color(0,0,0))
+		g.fillRoundRect(0,0,30,30,10,10)
+	def mousePressed(self, locationOnScreen, g) :
+		self.start_point = locationOnScreen
+		return ""
+	def mouseHovered(self, locationOnScreen, g) :
+		return ""
+	def mouseDragged(self, locationOnScreen, g) :
+		x1 = min(self.start_point.x, locationOnScreen.x)
+		y1 = min(self.start_point.y, locationOnScreen.y)
+		x2 = max(self.start_point.x, locationOnScreen.x)
+		y2 = max(self.start_point.y, locationOnScreen.y)
+		g.setColor(self.client_state.selectedColor)
+		g.fillRoundRect(x1, y1, x2 - x1, y2 - y1,20,20)
+		return ""
+	def mouseReleased(self, locationOnScreen, g) :
+		x1 = min(self.start_point.x, locationOnScreen.x)
+		y1 = min(self.start_point.y, locationOnScreen.y)
+		x2 = max(self.start_point.x, locationOnScreen.x)
+		y2 = max(self.start_point.y, locationOnScreen.y)
+		return self._getmetadata() + "|" + "(%d,%d);(%d,%d) " % (x1, y1, x2 - x1, y2 - y1)
+	def _getmetadata(self) :
+		return "%d" % self.client_state.selectedColor.getRGB()
+	def draw(self, s, g) :
+		if s == "" :
+			return
+		metadata, points = s.split('|')
+		points = points.split(';')
+		g.setColor(Color(int(metadata)))
+		match = point_re.match(points[0])
+		if match is None:
+			return
+		x1, y1 = match.groups()
+		x1, y1 = int(x1), int(y1)
+		match = point_re.match(points[1])
+		if match is None:
+			return
+		x2, y2 = match.groups()
+		x2, y2 = int(x2), int(y2)
+		g.fillRoundRect(x1, y1, x2, y2,20,20)
+	def getAffectedArea(self, phrase) :
+		points = phrase.split('|')[1].split(';')
+		match = point_re.match(points[0])
+		if match is None:
+			return
+		x1, y1 = match.groups()
+		x1, y1 = int(x1), int(y1)
+		match = point_re.match(points[1])
+		if match is None:
+			return
+		x2, y2 = match.groups()
+		x2, y2 = int(x2), int(y2)
+		ax1 = min(x1, x2)
+		ay1 = min(y1, y2)
+		ax2 = max(x1, x2)
+		ay2 = max(y1, y2)
+		return [ax1, ay1, ax2 - ax1 + 1, ay2 - ay1 + 1]
+	def getIcon(self) :
+		return self.image
+	def getToolName(self) :
+		return "Filled Rounded Rectangle"
+	def getTooltip(self) :
+		return "draw a filled rounded rectangle with one corner\nwhere you click and the opposite\ncorner where you release the mouse"
+	def getToolID(self) :
+		return 'fillRoundRect'
+
 def _get_tools() :
 	#hacky way to get the list of tools
 	tools = []
