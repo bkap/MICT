63879551fe690dcf057d2250f1f12044b38694cf
Ben Kaplan <benjamin.kaplan@case.edu>
2010-11-22 17:08:59 -0500

    started working on handling serialization by hashing and transmitting files.

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,12 +1,47 @@
 from mict.tools import Tool
+import os.path
+import hashlib
+def get_tool_files_and_hashes() :
+	tools = []
+	tooldir = os.path.dirname(__file__)
+	for f_name in os.listdir(tooldir) :
+		if f_name.endswith('.py') and f_name != "__init__.py" :
+			tools.append(f_name + ';' + hashlib.sha1(open(os.path.join(tooldir, f_name),'r').read()).hexdigest())
+	return ':'.join(tools)
+tools = []
+def check_files(filestr) :
+	global tools
+	tooldir = os.path.dirname(__file__)
+	needed = []
+	for tool in filestr.split(':')
+		tool_file, sha1 = tool.split(';',1)
+		tool_f = os.path.join(tooldir, tool_file)
+		if not os.path.exists(os.path.join(tooldir,tool_file)) or hashlib.sha1(open(tool_f)).hexdigest() != sha1 :
+			needed.append(tool_file)
+		else :
+			tools.extend(load_tools(tool_file))
+	return ':'.join(needed)
+def load_tools(filename) :
+	tools = []
+	module = os.path.splitext(filename)[0]
+	toolmodule = __import__("tools." + module,fromlist=['__name__'])
+	for name in dir(toolmodule) :
+			obj = getattr(toolmodule, name)
+			if isinstance(obj, type) and issubclass(obj, Tool) and not obj == Tool:
+				obj.__file__ = filename
+				tools.append(obj)
+	return tools
 
-
-
-
-
-
-
-
+def add_file(filestr) :
+	filename, filetxt = filestr.split(':',1)
+	tooldir = os.path.dirname(__file__)
+	toolfile = os.path.join(tooldir, filename)
+	if os.path.exists(toolfile):
+		os.remove(toolfile)
+	f = open(toolfile,'w')
+	f.write(filetxt)
+	f.close()
+	tools.extend(load_tools(filename))
 def _get_tools() :
 	#hacky way to get the list of tools
 	tools = []
@@ -23,18 +58,8 @@ def _get_tools() :
 		print filename
 		if filename.endswith('.py') and filename != "__init__.py" :
 			#import the file
-			module = os.path.splitext(filename)[0]
-			print module
-			toolmodule = __import__("tools." + module,fromlist=['__name__'])
-			print dir(toolmodule)
-			for name in dir(toolmodule) :
-				print name
-				obj = getattr(toolmodule, name)
-				if isinstance(obj, type) and issubclass(obj, Tool) and not obj == Tool:
-					obj.__file__ = filename
-					tools.append(obj)
+			tools.extend(load_tools(filename))
 	return tools
-tools = _get_tools()
 def get_tool_definitions(toolnames) :
 	'''returns the text of all tool files
 	for the tools with the given names. If the tool is defined in __init__.py,
