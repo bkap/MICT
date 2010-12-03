f61aa79dd49783bc95c8bf94ca8ba2768f84a166
Rob Wiesler <halcyone1024@gmail.com>
2010-12-02 21:37:08 -0500

    Did some pointless things to ImageTest.

--- mict/test/ImageTest.java
+++ mict/test/ImageTest.java
@@ -3,7 +3,17 @@ package mict.test;
 import java.awt.*;
 import javax.swing.*;
 
-public class ImageTest extends JPanel {
+public class ImageTest extends JPanel implements Runnable {
+	public static void popupAndDie(Image img) {
+		JFrame jf = new JFrame("RDE::IMAGE_TEST");
+		JPanel jp = new ImageTest(img);
+		jf.setSize(500, 500);
+		jf.getContentPane().add(jp);
+		jf.setVisible(true);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jp.repaint();
+	}
+
 	public static void popup(Image img) {
 		JFrame jf = new JFrame("RDE::IMAGE_TEST");
 		JPanel jp = new ImageTest(img);
@@ -23,5 +33,16 @@ public class ImageTest extends JPanel {
 	public void paint(Graphics g) {
 		System.out.println("Image" + id + ": " + img);
 		g.drawImage(img, 0, 0, this);
+	}
+
+	public void start() {
+		new Thread(this).start();
+	}
+
+	public void run() {
+		setVisible(true);
+		while(true) {
+			try { Thread.sleep(100); } catch(InterruptedException e) {}
+		}
 	}
 }
